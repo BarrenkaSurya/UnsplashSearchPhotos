c1e12667d6c8ec79bf231be3e604b334eb944115
Surya <barrenkasurya@gmail.com>
2023-07-28 21:31:24 +0530

    js project

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let maxImages = 12;
 let currentPage = 1;
+let lastPage = 0; 
 let apiKey = "9HQ9pYvsQGQ_b2Ou8xY31m7ZVd-GjOMo23-5da_mngQ" ;
-
-
-
 
 
 const searchbtn = document.getElementById("searchBtn");
@@ -38,7 +36,7 @@ async function makeSearch(){
         const photographerPage = eResp.user.links.html ;
 
         htmlContent +=`
-            <div>
+            <div class="card">
                 <a  href="${unSpalshLink}" target="_blank">
                     <div class="result-item" style="background-image : url(${url})"></div>           
                 </a>
@@ -49,6 +47,52 @@ async function makeSearch(){
         `
     });
 
+    lastPage = responce.total_pages ;
+
     const imageContainer = document.getElementById("imagecontainer") ;
     imageContainer.innerHTML = htmlContent ;
+
+
+    const infopara = document.getElementById("infopara");
+    infopara.innerText =  `About ${responce.total} results found` ;
+
+    const countinfoPara = document.getElementById("countinfoPara");
+
+    let startPoint = ((currentPage - 1) *maxImages) + 1 ;
+    let endPoint = maxImages * currentPage ; 
+    countinfoPara.innerText = `${startPoint} - ${endPoint} of page ${currentPage}`
+    updateBtnState() ;
+}
+
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+nextBtn.onclick = nextPage ;
+prevBtn.onclick = prevPage ;
+
+function updateBtnState(){
+    nextBtn.classList.remove("hidden")
+    if(currentPage >= lastPage){
+        nextBtn.classList.add("hidden")
+    }
+    prevBtn.classList.remove("hidden")
+    if(currentPage == 1){
+        prevBtn.classList.add("hidden")
+    }
+}
+
+function nextPage(){
+    if(lastPage < currentPage){
+        return ;
+    }
+    currentPage++;
+    makeSearch(); 
+}
+
+function prevPage(){
+    if(currentPage == 1){
+        return ;
+    }
+    currentPage--;
+    makeSearch(); 
 }
